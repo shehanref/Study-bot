a9fa7f3d233e8c3976ba486e92af5be37b131762
shehanref <asshehan5@gmail.com>
2026-02-26 22:20:05 +0600

    Update studybot.py

--- studybot.py
+++ studybot.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import sqlite3
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
